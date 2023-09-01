a22e350fae4d1b468859bc870476f5b25d989dcd
igovor01 <igovor01@fesb.hr>
2023-09-01 14:33:30 +0200

    layout first change

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+function Footer(){
+    return (
+    <footer className="bg-orange-50 text-center dark:bg-neutral-700 lg:text-">
+        <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
+          © 2023 Copyright:
+          <a
+            className="text-neutral-800 dark:text-neutral-400"
+            href="/"
+            > IG</a>
+        </div>
+    </footer>
+)
+}
+
+export default Footer;
