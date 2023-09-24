1110b802b9fe562b48aa70cbe75df2e3a164843d
igovor01 <igovor01@fesb.hr>
2023-09-25 00:23:31 +0200

    updated nav; more  functional

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { NavbarItems } from "@/config/NavbarItems";
 import { FaRegUserCircle } from 'react-icons/fa';
+import { HiMenu } from 'react-icons/hi';
 import React, { useState, useEffect } from "react";
 function Navbar() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -30,7 +31,7 @@ function Navbar() {
           </span>
         </a>
 
-        <div className="flex items-center md:order-2">
+        {/* <div className="flex items-center md:order-2">
           <button
             type="button"
             className="flex mr-3 text-sm bg-white rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
@@ -45,7 +46,7 @@ function Navbar() {
               alt="user icon"
             />
           </button>
-          {/*<!-- Dropdown menu --> */}
+          
           <div
             className="z-50 hidden my-4 text-base list-none bg-orange-50 divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
             id="user-dropdown"
@@ -109,6 +110,54 @@ function Navbar() {
               />
             </svg>
           </button>
+        </div> */}
+        <div className="flex items-center md:order-2">
+        <div className="relative ">
+              <button
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                onClick={toggleDropdown}
+                aria-expanded={isDropdownOpen}
+              >
+                <span className="sr-only">Open user menu</span>
+            <FaRegUserCircle
+              className="w-8 h-8 rounded-full"
+              alt="user icon"
+            />
+              </button>
+              {isDropdownOpen && (
+                <ul className="absolute right-0 z-50 my-4 text-base list-none bg-orange-50 divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+                  <li>
+                    <Link
+                      href="/login"
+                      className="block w-32 px-4 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    >
+                      Log in
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/signup"
+                      className="block w-32 w-fill px-4 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    >
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </div>
+            <button
+            data-collapse-toggle="navbar-user"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-user"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <HiMenu
+              className="w-8 h-8"
+              aria-hidden="true" 
+            />
+          </button>
         </div>
         <div
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 bg-orange-50"
@@ -125,35 +174,7 @@ function Navbar() {
               </Link>
             ))}
 
-            <div className="relative">
-              <button
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                onClick={toggleDropdown}
-                aria-expanded={isDropdownOpen}
-              >
-                Dropdown
-              </button>
-              {isDropdownOpen && (
-                <ul className="absolute z-50 my-4 text-base list-none bg-orange-50 divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
-                  <li>
-                    <Link
-                      href="/login"
-                      className="block px-4 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      Log in
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/signup"
-                      className="block w-fill px-4 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      Sign up
-                    </Link>
-                  </li>
-                </ul>
-              )}
-            </div>
+            
           </div>
         </div>
       </div>
