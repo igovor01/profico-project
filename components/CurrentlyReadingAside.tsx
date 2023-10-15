1c8c52248a645ca7d91ca1cf4f8e369139b2a9e9
igovor01 <igovor01@fesb.hr>
2023-10-15 19:21:02 +0200

    one change

--- components/CurrentlyReadingAside.tsx
+++ components/CurrentlyReadingAside.tsx
@@ -3,7 +3,7 @@ import BookCurrentElement from "./BookCurrentElement";
 
 function CurrentlyReadingAside() {
   return (
-    <aside className="flex flex-col w-1/3 items-start h-screen p-4 overflow-hidden bg-white h-fit mt-9 rounded-md shadow-xl">
+    <aside className="flex flex-col w-1/3 items-start p-4 overflow-hidden bg-white h-fit mt-9 rounded-md shadow-xl">
       <span className="text-center text-md text-gray-800 m-0.5">
         CURRENTLY READING
       </span>
